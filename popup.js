d60c6569cdaf06b177f2296edda3e561a6a4b88a
Manabu Morita <tsuwako@gmail.com>
2012-05-10 00:54:26 +0900

    fundamental implementation

--- popup.js
+++ popup.js
@@ -1,1 +1,11 @@
-alert('hello, world');
+chrome.windows.getAll({populate: true}, function(windows) {
+  for(var i in windows) {
+    for(var j in windows[i].tabs) {
+      document.body.innerHTML += windows[i].tabs[j].title
+                               + '<br/>'
+                               + windows[i].tabs[j].url
+                               + '<br/><br/>';
+    }
+  }
+});
+
