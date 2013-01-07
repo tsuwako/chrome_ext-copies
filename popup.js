cff370eea5ab3d20d876ed3b4678199d31e06bf5
Manabu Morita <tsuwako@gmail.com>
2013-01-08 08:40:00 +0900

    add bookmark support

--- popup.js
+++ popup.js
@@ -1,10 +1,26 @@
+function createBookmarkElement(tab) {
+  var titleElem = document.createElement('span');
+  titleElem.innerHTML = tab.title;
+  var urlElem = document.createElement('span');
+  urlElem.innerHTML = tab.url;
+
+  var divElem = document.createElement('div');
+  divElem.appendChild(titleElem);
+  divElem.appendChild(urlElem);
+
+  divElem.addEventListener('click', function(ev) {
+    this.style.color = 'red';
+    chrome.bookmarks.create({'title': this.childNodes[0].innerHTML, 'url': this.childNodes[1].innerHTML});
+  }, false);
+
+  return divElem;
+}
+
 chrome.windows.getAll({populate: true}, function(windows) {
   for(var i in windows) {
     for(var j in windows[i].tabs) {
-      document.body.innerHTML += windows[i].tabs[j].title
-                               + '<br/>'
-                               + windows[i].tabs[j].url
-                               + '<br/><br/>';
+      var elem = createBookmarkElement(windows[i].tabs[j]);
+      document.body.appendChild(elem);
     }
   }
 });
